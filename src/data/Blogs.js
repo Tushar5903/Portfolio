8d5a23c4891ed61c769b296a121a7748ab806b74
Tushar Kumar <164053903+Tushar5903@users.noreply.github.com>
2026-03-26 18:20:02 +0530

    Update Blogs.js

--- src/data/Blogs.js
+++ src/data/Blogs.js
@@ -1,87 +1,120 @@
 const data = [
-  {
-    
-    id: '1',
-    title: 'Mastering Object-Oriented Programming in Java',
-    image: {
-      src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1761557946/Screenshot_2025-10-26_172533_il2aly.png',
-    },
-    links: {
-      site: 'https://www.linkedin.com/pulse/mastering-object-oriented-programming-java-tushar-kumar-9mp9c/?trackingId=lNHL0f7qbZpAKCK3k2l5qA%3D%3D',
-    },
-  },{
-    
-    id: '2',
-    title: 'Fetching Data from APIs in React',
-    image: {
-      src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1761557904/Screenshot_2025-10-27_150812_omb4en.png',
-    },
-    links: {
-      site: 'https://www.linkedin.com/pulse/fetching-data-from-apis-react-tushar-kumar-wremc/?trackingId=6JosYZgKec1QLBh5ZFx%2Bvw%3D%3D',
-    },
-  },{
-    
-    id: '3',
-    title: 'The Easy Way to Build Backends with Express.js',
-    image: {
-      src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1761557797/Screenshot_2025-10-27_150624_weuwb6.png',
-    },
-    links: {
-      site: 'https://www.linkedin.com/pulse/easy-way-build-backends-expressjs-tushar-kumar-wbqtc/?trackingId=hen%2B3WXqNo5PahCVoXNo4Q%3D%3D',
-    },
-  },
-  {
-    
-    id: '4',
-    title: 'Swiper Element Guide for React',
-    image: {
-      src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1761557255/Screenshot_2025-10-27_145709_dhcd2w.png',
-    },
-    links: {
-      site: 'https://www.linkedin.com/pulse/swiper-element-guide-react-tushar-kumar-hnq8c/?trackingId=rEQIESuK7XOJ0TyVHys9yw%3D%3D',
-    },
-  },
-  {
-    id: '5',
-    title: 'Building Forms in React: A Modern Guide for Developers',
-    image: {
-      src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1761557613/Screenshot_2025-10-27_150319_qzbwuo.png',
-    },
-    links: {
-      site: 'https://www.linkedin.com/pulse/building-forms-react-modern-guide-developers-tushar-kumar-sbolc/?trackingId=yUalrz5BEAakp5Nr9LsesQ%3D%3D',
-    },
-  },
-  {
-    id: '6',
-    title: 'How much JavaScript is required to get started with reactJS?',
-    image: {
-      src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1752737440/Screenshot_2025-07-09_123906_gy0rmx.png',
-    },
-    links: {
-      site: 'https://www.linkedin.com/pulse/how-much-javascript-required-get-started-reactjs-tushar-kumar-4burc/?trackingId=7ZcKHAm3Z%2BlHUxJggYx0AA%3D%3D',
-    },
-  },
-  {
-    id: '7',
-    title: 'Understanding REDUX & REACT REDUX State Management in React',
-    image: {
-      src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1754579897/Screenshot_2025-07-22_133633_ee7jga.png',
-    },
-    links: {
-      site: 'https://www.linkedin.com/pulse/understanding-redux-react-state-management-tushar-kumar-fn6nc/?trackingId=5gsQWGyQyeEvaOPx1Gh4uA%3D%3D',
-    },
-  },
-    {
-    id: '8',
-    title: 'Mastering React Hooks',
-    image: {
-      src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1754580035/Screenshot_2025-08-02_181138_bjokpt.png',
-    },
-    links: {
-      site: 'https://www.linkedin.com/pulse/mastering-react-hooks-tushar-kumar-rerzc/?trackingId=%2Bisi64SgqJatOpWR%2BS4ReA%3D%3D',
-    },
-  },
-    
+
+{
+
+id: '1',
+title: 'Mastering the Java Collections Framework: A Deep Dive into Logic, Hierarchy, and Performance',
+image: {
+src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1774529312/Screenshot_2026-03-26_181821_icdxud.png',
+},
+links: {
+site:
+'https://www.linkedin.com/pulse/mastering-java-collections-framework-deep-dive-logic-hierarchy-kumar-jgavc/?trackingId=wOF4N32z5m1E2arjX3DEbw%3D%3D',
+},
+},
+{
+
+id: '2',
+title: 'Why Your Java Code Needs Better Exception Handling',
+image: {
+src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1774528909/Screenshot_2026-03-26_181047_h6v3bb.png',
+},
+links: {
+site:
+'https://www.linkedin.com/pulse/why-your-java-code-needs-better-exception-handling-tushar-kumar-tjb8c/?trackingId=6LMkbWTpCMAWCIjrQRnoyw%3D%3D',
+},
+},
+{
+
+id: '3',
+title: 'Mastering Object-Oriented Programming in Java',
+image: {
+src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1761557946/Screenshot_2025-10-26_172533_il2aly.png',
+},
+links: {
+site:
+'https://www.linkedin.com/pulse/mastering-object-oriented-programming-java-tushar-kumar-9mp9c/?trackingId=lNHL0f7qbZpAKCK3k2l5qA%3D%3D',
+},
+},{
+
+id: '4',
+title: 'Fetching Data from APIs in React',
+image: {
+src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1761557904/Screenshot_2025-10-27_150812_omb4en.png',
+},
+links: {
+site:
+'https://www.linkedin.com/pulse/fetching-data-from-apis-react-tushar-kumar-wremc/?trackingId=6JosYZgKec1QLBh5ZFx%2Bvw%3D%3D',
+},
+},{
+
+id: '5',
+title: 'The Easy Way to Build Backends with Express.js',
+image: {
+src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1761557797/Screenshot_2025-10-27_150624_weuwb6.png',
+},
+links: {
+site:
+'https://www.linkedin.com/pulse/easy-way-build-backends-expressjs-tushar-kumar-wbqtc/?trackingId=hen%2B3WXqNo5PahCVoXNo4Q%3D%3D',
+},
+},
+{
+
+id: '6',
+title: 'Swiper Element Guide for React',
+image: {
+src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1761557255/Screenshot_2025-10-27_145709_dhcd2w.png',
+},
+links: {
+site:
+'https://www.linkedin.com/pulse/swiper-element-guide-react-tushar-kumar-hnq8c/?trackingId=rEQIESuK7XOJ0TyVHys9yw%3D%3D',
+},
+},
+{
+id: '7',
+title: 'Building Forms in React: A Modern Guide for Developers',
+image: {
+src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1761557613/Screenshot_2025-10-27_150319_qzbwuo.png',
+},
+links: {
+site:
+'https://www.linkedin.com/pulse/building-forms-react-modern-guide-developers-tushar-kumar-sbolc/?trackingId=yUalrz5BEAakp5Nr9LsesQ%3D%3D',
+},
+},
+{
+id: '8',
+title: 'How much JavaScript is required to get started with reactJS?',
+image: {
+src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1752737440/Screenshot_2025-07-09_123906_gy0rmx.png',
+},
+links: {
+site:
+'https://www.linkedin.com/pulse/how-much-javascript-required-get-started-reactjs-tushar-kumar-4burc/?trackingId=7ZcKHAm3Z%2BlHUxJggYx0AA%3D%3D',
+},
+},
+{
+id: '9',
+title: 'Understanding REDUX & REACT REDUX State Management in React',
+image: {
+src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1754579897/Screenshot_2025-07-22_133633_ee7jga.png',
+},
+links: {
+site:
+'https://www.linkedin.com/pulse/understanding-redux-react-state-management-tushar-kumar-fn6nc/?trackingId=5gsQWGyQyeEvaOPx1Gh4uA%3D%3D',
+},
+},
+{
+id: '10',
+title: 'Mastering React Hooks',
+image: {
+src: 'https://res.cloudinary.com/dyaz7lhkb/image/upload/v1754580035/Screenshot_2025-08-02_181138_bjokpt.png',
+},
+links: {
+site:
+'https://www.linkedin.com/pulse/mastering-react-hooks-tushar-kumar-rerzc/?trackingId=%2Bisi64SgqJatOpWR%2BS4ReA%3D%3D',
+},
+},
+
 ];
 
 export default data;
